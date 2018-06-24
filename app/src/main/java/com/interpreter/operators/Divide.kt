272017de507d18bce6755ef7444d7c819ed41d19
roshan2M <r2munjal@edu.uwaterloo.ca>
2018-06-23 22:50:12 -0400

    Support multiplication and division.

---
+++ app/src/main/java/com/interpreter/operators/Divide.kt
@@ -0,0 +1,21 @@
+package com.interpreter.operators
+
+class Divide : Expression {
+
+    private lateinit var leftExpression: Expression
+    private lateinit var rightExpression: Expression
+
+    constructor(left: Expression, right: Expression) {
+        leftExpression = left
+        rightExpression = right
+    }
+
+    override fun interpret(): Double {
+        var denominator: Double = rightExpression.interpret()
+        if (denominator == 0.0) {
+            throw Exception("Invalid division expression. Denominator must be non-zero.")
+        }
+        return leftExpression.interpret() / denominator
+    }
+
+}
